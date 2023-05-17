5dd0a0311da3ab0f7ec8beb8c82786ea37bc2614
weixu <xuwei19850423@gmail.com>
2023-05-17 16:33:55 +1000

    show user

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,22 @@
 package com.example.demo;
 
+import java.util.Collections;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
